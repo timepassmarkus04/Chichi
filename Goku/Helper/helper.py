a6182326a9f8f0da5f3286be7cdab9fabc747713
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-07-26 14:57:00 +0530

    Add files via upload

---
+++ Goku/Helper/helper.py
@@ -0,0 +1,48 @@
+START_STIKERS = [
+    "CAACAgIAAxkBAAM8ZJM-4YzXU-_yTyk2m73D9vohpXYAAuUrAAJjCvlJzcmQ4GLMbX4vBA",
+    "CAACAgIAAxkBAAM-ZJM-5RhfcXlO-TH-1B6dQclaJKEAAkkvAALnYMBJ2MsGH8xIy0svBA",
+    "CAACAgIAAxkBAAM_ZJM-6PGkJN2ejoYw9b12QSltxSkAAl8tAAI4V_lJfGkzwLW4ubkvBA",
+    "CAACAgIAAxkBAANAZJM-6hZygx1uKzD7B-g9ePQLZIsAAmIqAAISQchJfJYxIEYPWuQvBA",
+    "CAACAgIAAxkBAANAZJM-6hZygx1uKzD7B-g9ePQLZIsAAmIqAAISQchJfJYxIEYPWuQvBA",
+    "CAACAgIAAxkBAANCZJM-7lyus69tnVznMHzN8yYa17YAAistAAIW1thJ66oLQhUnETEvBA"
+]
+
+
+PHOTO = [
+    "https://telegra.ph/file/b338214b85fee0c81aec5.jpg",
+    "https://telegra.ph/file/8e6f8bad1d448e3398468.jpg",
+    "https://telegra.ph/file/28296667cb727005c5f9e.jpg",
+    "https://telegra.ph/file/ae35d8a131d0e175c0e49.jpg",
+]
+
+PM_PHOTO = [
+    "https://te.legra.ph/file/e833e8e51f8f141f05b89.jpg",
+    "https://telegra.ph/file/f3f2fbc1f4fe27be928b0.jpg",
+    "https://telegra.ph/file/fb2d9535da92eb4e1cce1.jpg",
+    "https://telegra.ph/file/0549ae4812a41b4d76e9b.jpg",
+    "https://telegra.ph/file/f6edd6a18ea0dea31ba5f.jpg",
+    "https://telegra.ph/file/b6448fa7b7a1b7ff89faa.jpg",
+    "https://telegra.ph/file/6e6c13e103826fedba7c6.jpg",
+    "https://telegra.ph/file/e50b360220004444350fd.jpg",
+]
+ 
+START_VIDEO = [
+"https://telegra.ph/file/e374ba92cd333abe12c8a.mp4",
+"https://telegra.ph/file/39a18d24aa651ef6497af.mp4",
+"https://telegra.ph/file/96fed135fe1820925555e.mp4",
+"https://telegra.ph/file/c38a9d63ac2355075a534.mp4",
+"https://telegra.ph/file/ed6111ed26510b0af4357.mp4",
+"https://telegra.ph/file/bbdaf08bbefc3df7f81c9.mp4",
+"https://telegra.ph/file/65df8a4d8df645b9b5be9.mp4",
+"https://telegra.ph/file/dab74ea839ad9ffdfc12b.mp4",
+"https://telegra.ph/file/52241f62d8bc991605e6e.mp4",
+"https://telegra.ph/file/047f6761ebfbf4f6539d2.mp4",
+]
+
+SHIPPING_IMG = [
+    "https://telegra.ph/file/060870869e22fb1b26ae4.jpg",
+    "https://telegra.ph/file/5054396b76bab9d9e2bf1.jpg",
+    "https://telegra.ph/file/34de955bacb089438c089.jpg",
+    "https://telegra.ph/file/e7124ce4bc6e38a6bc76a.jpg",
+    "https://telegra.ph/file/4a5a484343a3a2a051fbd.jpg",
+]
